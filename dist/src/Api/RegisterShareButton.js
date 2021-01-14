c87aefe7c1171d7417566d5f674172084e82e11e
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-01-14 09:51:17 +0100

    fix: update dist

--- dist/src/Api/RegisterShareButton.js
+++ dist/src/Api/RegisterShareButton.js
@@ -1,12 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+const uuid_1 = require("uuid");
 const sendEvent_1 = require("../IO/sendEvent");
+const subscribeToEvent_1 = require("../IO/subscribeToEvent");
 exports.default = {
     register: ({ label, icon, onClick }) => {
+        const uuid = uuid_1.v4();
         sendEvent_1.default({
             action: 'register-share-button',
-            data: { label, icon }
+            data: { label, icon, id: uuid }
         });
+        subscribeToEvent_1.default(`register-share-button-${uuid}`, () => onClick());
     }
 };
 //# sourceMappingURL=RegisterShareButton.js.map
