48f7723035a9325c4711e0e5fe68253bb84d963f
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-02-04 10:25:21 +0100

    feat: add ability to handle custom images for some actions

--- src/Api/RegisterShareButton.ts
+++ src/Api/RegisterShareButton.ts
@@ -3,15 +3,16 @@ import sendEvent from '../IO/sendEvent'
 import subscribeToEvent from '../IO/subscribeToEvent'
 
 export default {
-  register: ({ label, icon, onClick }: {
+  register: ({ label, icon, imageSource, onClick }: {
     label: string,
     icon?: string,
+    imageSource?: string,
     onClick: () => unknown
   }): void => {
     const uuid = uuidv4()
     sendEvent({
       action: 'register-share-button',
-      data:  { label, icon, id: uuid }
+      data:  { label, icon, imageSource, id: uuid }
     })
 
     subscribeToEvent(`register-share-button-${uuid}`, () => onClick())
--- src/Api/RegisterStageButton.ts
+++ src/Api/RegisterStageButton.ts
@@ -3,11 +3,12 @@ import sendEvent from '../IO/sendEvent'
 import subscribeToEvent from '../IO/subscribeToEvent'
 
 export default {
-  register: ({ iframe, dropdownActions, dropdownActionsTextClasses, label, icon, tooltip, onClick }: {
+  register: ({ iframe, dropdownActions, dropdownActionsTextClasses, imageSource, label, icon, tooltip, onClick }: {
     label?: string,
     tooltip?: string,
     icon?: string,
-    dropdownActions?: Array<{ name?: string, label: any }>,
+    imageSource?: string,
+    dropdownActions?: Array<{ name?: string, label: any, imageSource?: string }>,
     dropdownActionsTextClasses?: string,
     iframe?: string
     onClick?: (payload?: any) => unknown
@@ -15,7 +16,7 @@ export default {
     const uuid = uuidv4()
     sendEvent({
       action: 'register-stage-button',
-      data:  { label, icon, tooltip,  id: uuid, iframe, dropdownActions, dropdownActionsTextClasses }
+      data:  { label, icon, imageSource, tooltip,  id: uuid, iframe, dropdownActions, dropdownActionsTextClasses }
     })
 
     subscribeToEvent(`register-stage-button-${uuid}`, (data) => onClick(data))
