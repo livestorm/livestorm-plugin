fe5dac6cd0805e952df5f3241adc182bb9f51926
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-02-09 17:45:49 +0100

    feat: add better interfaces

--- dist/src/Api/Chat/Send.d.ts
+++ dist/src/Api/Chat/Send.d.ts
@@ -1,7 +1,17 @@
-interface Message {
+interface MessageParam {
+    user: {
+        firstName?: String;
+        lastName?: String;
+        tag?: String;
+        avatarUrl?: String;
+    };
+    text?: String;
+    html?: String;
+}
+interface MessageInstance {
     id: String;
     destroy: () => void;
     onIframeMessage: (Function: any) => void;
 }
-export default function Send(data: any): Message;
+export default function Send(data: MessageParam): MessageInstance;
 export {};
--- src/Api/Chat/Send.ts
+++ src/Api/Chat/Send.ts
@@ -2,13 +2,24 @@ import { v4 as uuidv4 } from 'uuid';
 import sendEvent from '../../IO/sendEvent'
 import subscribeToEvent from '../../IO/subscribeToEvent'
 
-interface Message {
+interface MessageParam {
+  user: {
+    firstName?: String,
+    lastName?: String,
+    tag?: String,
+    avatarUrl?: String
+  },
+  text?: String,
+  html?: String
+}
+
+interface MessageInstance {
   id: String,
   destroy: () => void,
   onIframeMessage: (Function) => void
 }
 
-export default function Send(data: any): Message {
+export default function Send(data: MessageParam): MessageInstance {
   const id = uuidv4()
   sendEvent({
     action: 'chat-send',
