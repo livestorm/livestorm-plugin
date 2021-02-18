1a016f649d88e744bc37cb1bbc76838310e224ff
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-02-18 16:45:30 +0100

    spec: verifying that internal variable processing works

--- __tests__/IO/processTemplate.spec.ts
+++ __tests__/IO/processTemplate.spec.ts
@@ -7,4 +7,11 @@ describe('processTemplate', () => {
     const processedTemplate = processTemplate(template, { var2: 'coucou' })
     expect(processedTemplate).toStrictEqual('<div>{{ var }} coucou</div>')
   })
+
+  it('adds internal variables on top', () => {
+    const template = '<div>{{ style }}</div>'
+
+    const processedTemplate = processTemplate(template, {})
+    expect(processedTemplate.includes('border-radius')).toStrictEqual(true)
+  })
 })
