2cdb35aa4e61628679418f6fb17a16d07399dede
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-02-18 17:03:58 +0100

    fix: removing css management from plugin

--- dist/src/IO/processTemplate.js
+++ dist/src/IO/processTemplate.js
@@ -1,9 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const baseStyle_1 = require("../Assets/baseStyle");
-function processTemplate(content, userVariables) {
+function processTemplate(content, variables) {
     let node = content;
-    const variables = Object.assign({ style: baseStyle_1.default }, userVariables);
     for (let variable in variables) {
         node = node.split(`{{${variable}}}`).join(variables[variable]);
         node = node.split(`{{ ${variable} }}`).join(variables[variable]);
--- src/IO/processTemplate.ts
+++ src/IO/processTemplate.ts
@@ -1,13 +1,6 @@
-import style from '../Assets/baseStyle'
-
-export default function processTemplate(content: string, userVariables: any) {
+export default function processTemplate(content: string, variables: any) {
   let node = content
   
-  const variables = {
-    style,
-    ...userVariables
-  }
- 
   for (let variable in variables) {
     node = node.split(`{{${variable}}}`).join(variables[variable]);
     node = node.split(`{{ ${variable} }}`).join(variables[variable]);
