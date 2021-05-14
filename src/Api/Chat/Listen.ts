3285b9ec9a99e390cb8b6062e98b838defa8711e
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-05-14 14:44:22 +0200

    feat: add ability to listen to message incoming from everyone

--- src/Api/Chat/Listen.ts
+++ src/Api/Chat/Listen.ts
@@ -9,14 +9,15 @@ import subscribeToEvent from '../../IO/subscribeToEvent'
   * @example Chat.listen(message => console.log(`Someone said ${message}`))
   *
   * @param callback - Function that will be called whenever the user posts a message
+  * @param options - A hash of options
   * 
 */
-export default function Listen(callback: (message: any) => void) {
+export default function Listen(callback: (message: any) => void, options?: { everyone?: boolean }) {
   const uuid = uuidv4()
   sendEvent({
-    action: 'chat-listen',
+    action: `chat-listen${options?.everyone ? '-everyone' : '' }`,
     data:  { id: uuid }
   })
 
-  subscribeToEvent(`chat-listener-${uuid}`, (data) => callback(data))
+  subscribeToEvent(`chat-listener-${options?.everyone ? 'everyone-' : '' }${uuid}`, (data) => callback(data))
 }
