c0913fee34bb5447acfd01317b54c443888ee8cd
jnevo <jeannoel.evo@gmail.com>
2021-05-31 11:25:02 +0200

    Delete src/IO directory

--- src/IO/sendEvent.ts
+++
@@ -1,9 +0,0 @@
-import Configuration from '@/configuration'
-
-export default function sendEvent<T extends Record<string, unknown>>(payload: { action: string, data: T }): void {
-  const messageData = {
-    pluginName: Configuration.pluginName,
-    ...payload
-  }
-  parent.postMessage(JSON.stringify(messageData), '*')
-}
--- src/IO/subscribeToEvent.ts
+++
@@ -1,6 +0,0 @@
-
-export default function subscribeToEvent<T extends Record<string, unknown>>(eventName: string, callback: (data: T) => void): void {
-  window.addEventListener('message', (message) => {
-    if (message.data.eventName === eventName) callback(message.data.data || message.data)
-  })
-}
