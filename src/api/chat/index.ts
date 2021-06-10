42012454d03085b9d5b308ebdb0b81bab6d0323a
Jean-Noël EVO <jeannoel.evo@gmail.com>
2021-06-10 11:11:03 +0200

    fix: remove chat registerShareButton from chat namespace

--- src/api/chat/index.ts
+++ src/api/chat/index.ts
@@ -2,7 +2,6 @@ import listen from './listen'
 import intercept from './intercept'
 import send from './send'
 import broadcast from './broadcast'
-import registerShareButton from './registerShareButton'
 import registerMessageAction from './registerMessageAction'
 
 export default {
@@ -10,6 +9,5 @@ export default {
   intercept,
   send,
   broadcast,
-  registerShareButton,
   registerMessageAction
 }
