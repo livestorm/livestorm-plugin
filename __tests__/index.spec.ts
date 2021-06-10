cb66dec5024ce007f29c200094bc4c5330074f7e
Jean-Noël EVO <jeannoel.evo@gmail.com>
2021-06-10 11:19:12 +0200

    test: check buttons is defined with its methods

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,9 +1,16 @@
-import index from '../src/index'
+import Livestorm from '../src/index'
 
 describe('index', () => {
   it('exports an object with all exposed apis', () => {
-    expect(index.PubSub.publish).not.toBeNull()
-    expect(index.PubSub.subscribe).not.toBeNull()
-    expect(index.RegisterShareButton.register).not.toBeNull()
+    // PubSub
+    expect(Livestorm.PubSub.publish).toBeDefined()
+    expect(Livestorm.PubSub.subscribe).toBeDefined()
+
+    // Buttons
+    expect(Livestorm.Buttons).toBeDefined()
+    expect(Livestorm.Buttons.registerChatShareButton).toBeDefined()
+    expect(Livestorm.Buttons.registerShareButton).toBeDefined()
+    expect(Livestorm.Buttons.registerStageButton).toBeDefined()
+    expect(Livestorm.Buttons.registerCameraEffectButton).toBeDefined()
   })
 })
