529b5840a04e5d21f322768af1403997ede8b7f5
Jean-Noël EVO <jeannoel.evo@gmail.com>
2021-06-10 11:06:47 +0200

    feat: register camera effect button

--- src/api/buttons.ts
+++ src/api/buttons.ts
@@ -80,3 +80,18 @@ export function registerStageButton(options: StageButtonOptions): StageButtonWra
     }
   }
 }
+
+/** Register an entry in the Camera effects panel.
+ *  Can be used to trigger any action (upload custom background...))
+ * 
+ * @exemple Buttons.registerCameraEffectButton({
+ *   label: 'Upload a custom image',
+ *   imageSource: 'http://image/image.png',
+ *   onClick: () => console.log('someone clicked this button')
+ * })
+ * 
+ * @param options - DefaultButtonOptions
+ */
+export function registerCameraEffectButton(options: DefaultButtonOptions): void {
+  defaultDispatch('register-camera-effect-button', options)
+}
