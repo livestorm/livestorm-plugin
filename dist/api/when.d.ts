cd321c6853639b1eb59281874f0463138ecb8234
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-06-21 16:26:10 +0200

    feat: add dist folder

--- dist/api/when.d.ts
+++ dist/api/when.d.ts
@@ -1,26 +1,50 @@
 /**
-    *
-    * Be notified when the event ends.
-    * The callback is not called if the user joins the event after the end.
-    *
-    * @example When.eventEnds(() => {
-    *  // do something
-    * })
-    *
-    * @param callback - A function to be called whenever the event is triggered
-    *
-  */
+  *
+  * Be notified when the event ends.
+  * The callback is not called if the user joins the event after the end.
+  *
+  * @example When.eventEnds(() => {
+  *  // do something
+  * })
+  *
+  * @param callback - A function to be called whenever the event is triggered
+  *
+*/
 export declare function eventEnds(callback: (params: Record<string, unknown>) => void): void;
 /**
-    *
-    * Be notified when the event starts.
-    * The callback is not called if the user joins the event after the start.
-    *
-    * @example When.eventStarts(() => {
-    *  // do something
-    * })
-    *
-    * @param callback - A function to be called whenever the event is triggered
-    *
-  */
+  *
+  * Be notified when the event starts.
+  * The callback is not called if the user joins the event after the start.
+  *
+  * @example When.eventStarts(() => {
+  *  // do something
+  * })
+  *
+  * @param callback - A function to be called whenever the event is triggered
+  *
+*/
 export declare function eventStarts(callback: (params: Record<string, unknown>) => void): void;
+/**
+  *
+  * Be notified when someone enters the room.
+  *
+  * @example When.userJoins(() => {
+  *  // do something
+  * })
+  *
+  * @param callback - A function to be called whenever the event is triggered
+  *
+*/
+export declare function userJoins(callback: (params: Record<string, unknown>) => void): void;
+/**
+  *
+  * Be notified when someone leaves the room.
+  *
+  * @example When.userJoins(() => {
+  *  // do something
+  * })
+  *
+  * @param callback - A function to be called whenever the event is triggered
+  *
+*/
+export declare function userLeaves(callback: (params: Record<string, unknown>) => void): void;
