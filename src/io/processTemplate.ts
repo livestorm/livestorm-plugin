8011a49fdce3f0d40f9d9bbaad154ba669ecc0b5
Jean-Noël EVO <jeannoel.evo@gmail.com>
2021-07-01 10:00:45 +0200

    refacto: restore to typescript 4.1.3

--- src/io/processTemplate.ts
+++ src/io/processTemplate.ts
@@ -1,8 +1,8 @@
 export default function processTemplate(content: string, variables: Record<string, unknown> = {}): string {
-  // Inject the variables in a the global __VARIABLES__
+  // Inject the variables in the global __VARIABLES__
   const injectedVariables = `<script type="text/javascript">window.__VARIABLES__ = ${JSON.stringify(variables)}</script>`
 
-  // Replace all occurences of {{ string }} with the variable (empty if not found)
+  // Replace all occurences of {{ variable }} with the variable value (empty if not found)
   const replacedOccurrences = content.replace(/({{)([0-9a-zA-Z-\s]+)(}})/g, (fullMatch, match1, match2) => {
     const variable = match2.trim()
     const value = variable in variables ? variables[variable] : ''
