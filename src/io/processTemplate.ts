aade9ced20af1c48a5f4342ca33bc6246884ffd5
Jean-Noël EVO <jeannoel.evo@gmail.com>
2021-07-01 10:45:10 +0200

    fix: throw error if JSON.stringify fails in processTemplate

--- src/io/processTemplate.ts
+++ src/io/processTemplate.ts
@@ -1,13 +1,24 @@
 export default function processTemplate(content: string, variables: Record<string, unknown> = {}): string {
   // Inject the variables in the global __VARIABLES__
-  const injectedVariables = `<script type="text/javascript">window.__VARIABLES__ = ${JSON.stringify(variables)}</script>`
+  let injectedVariables
+  try {
+    injectedVariables = `<script type="text/javascript">window.__VARIABLES__ = ${JSON.stringify(variables)}</script>`
+  } catch (e) {
+    throw new Error('The variables could not been injected: ' + e)
+  }
 
   // Replace all occurences of {{ variable }} with the variable value (empty if not found)
   const replacedOccurrences = content.replace(/({{)([0-9a-zA-Z-\s]+)(}})/g, (fullMatch, match1, match2) => {
     const variable = match2.trim()
     const value = variable in variables ? variables[variable] : ''
   
-    if (typeof value === "object") return JSON.stringify(value)
+    if (typeof value === "object") {
+      try {
+        return JSON.stringify(value)
+      } catch (e) {
+        throw new Error(`The variable "${variable}" could not been replaced in the template: ${e}`)
+      }
+    }
     return String(value)
   })
 
