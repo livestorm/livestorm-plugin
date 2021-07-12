a945315e7c49ad83ea4cd5fdf879ac32a3941e06
Jean-Noël EVO <jeannoel.evo@gmail.com>
2021-07-12 19:23:47 +0200

    ci: add roomEnter and logout commands

---
+++ cypress/support/commands.ts
@@ -0,0 +1,43 @@
+// eslint-disable-next-line @typescript-eslint/no-namespace
+declare namespace Cypress {
+  interface Chainable<Subject> {
+    /**
+     * Login then select the first event and enter the room
+     */
+    roomEnter(): Cypress.Chainable<null>;
+
+    /**
+     * Close the session of the user
+     */
+     logout(): Cypress.Chainable<null>;
+  }
+}
+
+Cypress.Commands.add('roomEnter', () => {
+  cy.visit('/')
+
+  cy.intercept('/api/v1/organizations/current').as('logged')
+
+  cy.get('[id^="BaseFormInput:email"]').type(Cypress.env('TEAM_MEMBER_EMAIL'))
+  cy.get('[id^="BaseFormInput:password"]').type(Cypress.env('TEAM_MEMBER_PASSWORD'))
+  cy.get('button[type="submit"]').click()
+
+  cy.wait('@logged')
+
+  cy.get('.data-view-item').click()
+
+  cy.get('[data-testid="access-event-room"]').invoke('attr', 'href').as('link')
+
+  cy.get('@link').then(link => {
+    cy.visit(link as unknown as string)
+  })
+
+  cy.get('.confirm-config-button:not([disabled])', {
+    timeout: 10000
+  }).click()
+
+})
+
+Cypress.Commands.add('logout', () => {
+  cy.visit('/#logout')
+})
