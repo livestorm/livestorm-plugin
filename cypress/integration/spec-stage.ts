c98cf26d13aca1017f83f03e8d1ee5c1064a554c
Jean-Noël EVO <jeannoel.evo@gmail.com>
2021-07-13 15:16:11 +0200

    ci: add stage fixture and add stage action button test

--- cypress/integration/spec-stage.ts
+++ cypress/integration/spec-stage.ts
@@ -6,9 +6,28 @@ after( () => {
   cy.logout()
 })
 
+
 describe('The stage', () => {
   it('successfully displays the share button', () => {
-    cy.get('.share-dropdown button').click()
-    cy.get('[data-testid="plugin-stage-buttons"]').contains('Foo:bar')
+    cy.get('.share-dropdown button', {
+      timeout: 10000
+    }).click()
+
+    cy.fixture('stage').then((stage => {
+      cy.get('[data-testid="plugin-stage-share-buttons"]', {
+        timeout: 10000
+      }).contains(stage.registerShareButton.label).click()
+    }))
+
+  })
+
+  it('successfully displays the stage action button', () => {
+    cy.fixture('stage').then((stage => {
+      cy.get(`[data-testid="plugin-stage-actions"] button[aria-label="${stage.registerStageButton.label}"] img`, {
+        timeout: 10000
+      }).as('stageActionButtonImage')
+  
+      cy.get('@stageActionButtonImage').invoke('attr', 'src').should('eq', stage.registerStageButton.imageSource)
+    }))
   })
 })
--- plugin/stage.ts
+++ plugin/stage.ts
@@ -1,11 +1,8 @@
 import { Stage } from '../dist/index'
 
+import fixtureStage from './../cypress/fixtures/stage.json'
+
 export default function stage (): void {
-  Stage.Buttons.registerShareButton({
-    label: 'Foo:bar',
-    icon: 'calendar',
-    onClick: () => {
-      console.log('buzz:bar')
-    }
-  })
+  Stage.Buttons.registerShareButton(fixtureStage.registerShareButton)
+  Stage.Buttons.registerStageButton(fixtureStage.registerStageButton)
 }
