8fc233b209d5d03e5e64364183398665ba7c4bc5
Michaël Villeneuve <mvilleneuve.pro@gmail.com>
2021-09-16 18:55:35 +0200

    feat: exposing a settings api on the organization level

--- src/api/settings/index.ts
+++ src/api/settings/index.ts
@@ -1,7 +1,9 @@
 import * as Event from '@/api/settings/event'
+import * as Organization from '@/api/settings/event'
 import register from '@/api/settings/register'
 
 export {
   Event,
+  Organization,
   register
 }
