a19d200a7f3b3ebdf228592300b687669a745128
Jean-Noël EVO <jeannoel.evo@gmail.com>
2021-10-18 18:59:11 +0200

    fix: add type for Listen and Intercept chat message, liv-4702 (#53)
    * fix: add type for Listen and Intercept chat message
    
    * feat(chat): add missing data for Chat Message Type

--- dist/api/chat/listen.d.ts
+++ dist/api/chat/listen.d.ts
@@ -1,3 +1,4 @@
+import { ChatMessage, ChatMessageWithUser, ChatListenOptions } from '../../types/chat';
 /**
  *
  * Be notified whenever someone posts a message in the chat.
@@ -8,6 +9,6 @@
  * @doc https://developers.livestorm.co/docs/chat#listen
  *
  */
-export default function Listen(callback: (message: Record<string, unknown>) => void, options?: {
-    everyone?: boolean;
-}): void;
+export default function Listen<U extends ChatListenOptions, T extends (U extends {
+    everyone: true;
+} ? ChatMessageWithUser : ChatMessage)>(callback: (message: T) => void, options?: U): void;
