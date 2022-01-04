8eb14af3ec87f8d68ca99c81c551c8336f5b1b25
Jean-Noël EVO <jeannoel.evo@gmail.com>
2022-01-04 11:07:08 +0100

    Update spec-modal.ts

--- cypress/integration/spec-modal.ts
+++ cypress/integration/spec-modal.ts
@@ -9,7 +9,7 @@ describe('The modal', () => {
   
   // @showIframe
   it('should display a modal with an iframe inside', () => {
-    cy.wait(3000)
+    cy.wait(1000)
     cy.fixture('modal').then((modal) => {
       cy.getIframeBody('modal').contains(modal.expectedElement, modal.expectedText)
     })
