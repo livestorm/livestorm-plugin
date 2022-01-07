bf8ffef5c202e052384c65a72bfd0fcb835d5ff4
Jean-Noël EVO <jeannoel.evo@gmail.com>
2022-01-07 18:38:34 +0100

    test: improve user

--- cypress/integration/spec-users.ts
+++ cypress/integration/spec-users.ts
@@ -2,31 +2,33 @@ describe('The users', () => {
   before(() => {
     cy.roomEnter(true)
   })
-  
-  after( () => {
+
+  after(() => {
     cy.logout()
   })
 
-  it('should return the users data', () => {
+  it('should return the current user data', () => {
     cy.getIframeWindow().should('exist')
 
-    // @me
     cy.getIframeWindow().should('have.property', 'ME')
     cy.fixture('users').then(({ withHost }) => {
-      cy.getIframeWindow().its('ME').should('have.keys', withHost)
+      cy.getIframeWindow().its('ME').should('have.all.keys', withHost)
     })
+  })
 
-
-    // @everyone
+  it('should return everyone data', () => {
     cy.getIframeWindow().should('have.property', 'EVERYONE')
     cy.fixture('users').then(({ user }) => {
       cy.getIframeWindow().its('EVERYONE').as('everyone')
       cy.get('@everyone').should('be.a', 'array')
       cy.get('@everyone').should('have.length', 1)
       cy.get('@everyone').then(users => {
-        expect(Object.keys(users[0])).to.be.deep.eq(user)
+        expect(Object.keys(users[0])).to.have.all.keys(user)
       })
     })
+  })
+
+  it('should return team members data', () => {
 
     // @teamMembers
     cy.getIframeWindow().should('have.property', 'TEAM_MEMBERS')
@@ -35,7 +37,7 @@ describe('The users', () => {
       cy.get('@teamMembers').should('be.a', 'array')
       cy.get('@teamMembers').should('have.length', 1)
       cy.get('@teamMembers').then(users => {
-        expect(Object.keys(users[0])).to.be.deep.eq(user)
+        expect(Object.keys(users[0])).to.have.all.keys(user)
       })
     })
   })
