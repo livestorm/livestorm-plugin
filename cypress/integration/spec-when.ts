21171e019dd96954f933081faca093a2136fa578
Jean-Noël EVO <jeannoel.evo@gmail.com>
2022-01-13 12:47:49 +0100

    fix: format

--- cypress/integration/spec-when.ts
+++ cypress/integration/spec-when.ts
@@ -2,18 +2,18 @@ describe('The When', () => {
   before(() => {
     cy.roomEnter()
   })
-  
-  after( () => {
+
+  after(() => {
     cy.logout(false)
   })
-  
+
   // @eventStarts
   it('should notify when the event start', () => {
     cy.getIframeContent('plugin', 'window').its('eventStarted').should('eq', false)
 
-    cy.get('button.start-button').click({ force: true})
+    cy.get('button.start-button').click({ force: true })
     cy.get('#modal-start').check({ force: true })
-    cy.get('.modal-start button').eq(1).click({ force: true})
+    cy.get('.modal-start button').eq(1).click({ force: true })
 
     cy.wait(12000)
     cy.get('.end-button', {
@@ -24,9 +24,9 @@ describe('The When', () => {
 
   // @eventEnds
   it('should notify when the event ends', () => {
-    cy.get('.end-button').click({ force: true})
+    cy.get('.end-button').click({ force: true })
     cy.get('.modal-end input[type="checkbox"]').check({ force: true })
-    cy.get('.modal-end button').eq(1).click({ force: true})
+    cy.get('.modal-end button').eq(1).click({ force: true })
     cy.getIframeContent('plugin', 'window').its('eventEnded').should('eq', true)
   })
 })
